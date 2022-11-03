5e608050786f57f8b1d63c02751d5461456518cf
Alexis Martinez <alexis4329@gmail.com>
2022-11-03 09:13:37 -0300

    vps config

--- api/src/bootstrap/server.bootstrap.js
+++ api/src/bootstrap/server.bootstrap.js
@@ -1,13 +1,37 @@
-var http = require("http");
+const http = require('http')
+const https = require('https');
+const fs = require('fs');
+const pem = require("pem");
 /* app setup */
 const PORT = process.env.PORT || 8080;
 const hostname = "127.0.0.1";
 
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/mejorobera.com.ar/privkey.pem','utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/mejorobera.com.ar/cert.pem','utf8')
+const ca = fs.readFileSync('/etc/letsencrypt/live/mejorobera.com.ar/chain/pem','utf8')
+const credentials = {
+    key:privateKey,
+    cert:certificate,
+    ca:ca
+}
+// const pfx = fs.readFileSync("src/certificate/mejorobera.com.ar.pfx");
+// pem.readPkcs12(pfx, { p12Password: "dattatec" }, (err, cert) => {
+//     console.log(cert);
+//});
+// const options = {
+//     pfx: fs.readFileSync( 'src/certificate/mejorobera.com.ar.pfx'),
+//     passphrase: ''
+//   };
+//  console.log(pfx)
 // ice factory
 const serverFunction = () => {
     async function initialize(app) {
         const promiseServer = new Promise((resolve, reject) => {
-        server = http.createServer(app);
+        server = http.createServer( app);
+        const httpsServer = https.createServer(credentials,app)
+        httpsServer.listen(4001, ()=>{
+            console.log('HTTPS Server running on port 4001')
+        })
         server
             .listen(PORT)
             .on("listening", () => {
